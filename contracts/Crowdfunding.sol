9cfaa058772b3e98fc740b9e2dc007e87f4f85a9
tencalink0 <bencax270@gmail.com>
2025-04-14 10:05:13 +0100

    feat: modifiers

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -2,5 +2,17 @@
 pragma solidity ^0.8.2;
 
 contract CrowdFunding {
+    struct Campaign {
+        address creator;
+        uint goal;
+        uint pledged;
+        uint32 deadline;
+        bool claimed;
+        mapping(address => uint) contributions;
+    }
+
+    // campaign id -> campaign 
+    mapping(uint => Campaign) public campaigns;
+
     
 }
--- contracts/Modifiers.sol
+++ contracts/Modifiers.sol
@@ -2,5 +2,18 @@
 pragma solidity ^0.8.2;
 
 contract Modifiers {
-    
+    modifier onlyCreator(address _creator) {
+        require(msg.sender == _creator, "Unauthorised access");
+        _;
+    }
+
+    modifier beforeDeadline(uint32 _deadline) {
+        require(block.timestamp < _deadline, "The deadline has passed");
+        _;
+    }
+
+    modifier afterDeadline(uint32 _deadline) {
+        require(block.timestamp > _deadline, "The deadline has not yet been reached");
+        _;
+    }
 }
