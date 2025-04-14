a953903248c8037e97566636b9f5fe21e24e6317
tencalink0 <bencax270@gmail.com>
2025-04-14 10:34:27 +0100

    feat: create campaing untested

--- contracts/Crowdfunding.sol
+++ contracts/Crowdfunding.sol
@@ -1,7 +1,9 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.2;
 
-contract CrowdFunding {
+import "./Modifiers.sol";
+
+contract CrowdFunding is Modifiers {
     struct Campaign {
         address creator;
         uint goal;
@@ -13,6 +15,16 @@ contract CrowdFunding {
 
     // campaign id -> campaign 
     mapping(uint => Campaign) public campaigns;
+    uint public campaignCount;
 
-    
+    function createCampaign(uint _goal, uint32 _deadline) external {
+        campaignCount++;
+
+        Campaign storage c = campaigns[campaignCount];
+        c.creator = msg.sender;
+        c.goal = _goal;
+        c.pledged = 0;
+        c.deadline = _deadline;
+        c.claimed = false;
+    }
 }
